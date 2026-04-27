3b3238a650c85ae89e63117d3e5bb28272811324
Daniel Wefer <z1998298@hopper.cs.niu.edu>
2026-04-27 15:37:20 -0500

    fixed table. make sure to create table again before filling

--- db_connect.php
+++ db_connect.php
@@ -3,9 +3,9 @@
 // Used In All Other .php Files
 // Through: require 'db_connect.php';
 $host = "courses";
-$db = "zID";
-$user = "zID"; // MariaDB
-$pass = "Password"; // MariaDB
+$db = "z1998298";
+$user = "z1998298"; // MariaDB
+$pass = "2004Apr21"; // MariaDB
 
 try{
 	$pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
